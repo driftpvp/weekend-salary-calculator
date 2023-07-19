df4ec957fa007813876b543c1dcb5be630b91dff
Lucas Martin <driftpvp@gmail.com>
2023-07-19 15:56:08 -0500

    need to look at another user assignment from another class to get month total to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,18 @@
 console.log("Hello World");
+// object
 let employ = {
     first: "Lucas",
     last: "Martin",
     id: 123,
     title: "Production",
-    salary: `$${1000}`,
+    annualSalary: `$${1000}`,
 }
 
+let employees = [];
+let totalMonthlyCost = 0;
+
+
+// add employee info to table
 function addEmployee(event){
     event.preventDefault();
     let firstName = document.querySelector("#first-name").value;
@@ -24,11 +30,22 @@ function addEmployee(event){
                 <td>$${salaryName}</td>
                 <td><button class="remove-employee" onclick="removeEmployee(event)">Delete</button></td>
             </tr>`;
-            employ.newEmployee.push("Bob", "Cratchet", 123, "accounting", "1000")
-            document.querySelector("#bio").reset();
-            return
+            employees.push(employ)
+            totalMonthlyCost += salaryName / 12;
+            updateCost();
+            console.log(totalMonthlyCost);
 }
+// had to get help for this part. looked at another user's assignment
+function updateCost(){
+    let annualSalaryUpdate = document.getElementById('TotalMonthCost');
+    annualSalaryUpdate.textContent = (totalMonthlyCost);
+    if (totalMonthlyCost > 20000) {
+        annualSalaryUpdate.classList.add('overBudget');
+    }
+}
+
 
 function removeEmployee(event){
     event.target.closest("tr").remove();
 }
+
